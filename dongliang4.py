cb9f0084dd8e81e81d59867e2f9b12c5d69a2aba
1412189000@qq.com <transformeris>
2022-02-17 16:32:33 +0800

    test connect

--- dongliang4.py
+++ dongliang4.py
@@ -23,34 +23,19 @@ etf_close=load_obj('etf_close')
 etf_all=pd.concat(etf_close, axis=1)
 etf_all=etf_all.sort_index()
 
+
+
+
 delta_etf_all=etf_all.shift(20)
 mtm_20=(etf_all-delta_etf_all)/etf_all
-mtm_20['max']=mtm_20.idxmax(axis=1)
-mtm_20['max_shift_1']=mtm_20['max'].shift(1)
-mtm_20_dropna=mtm_20.dropna(how='all')
-mtm_20_dropna=mtm_20_dropna.drop('2005-03-09')
-mtm_20_dropna=mtm_20_dropna.drop('2010-09-19')
-mtm_20_dropna=mtm_20_dropna.drop('2010-09-20')
-mtm_20_dropna=mtm_20_dropna.drop('2010-10-27')
-mtm_20_dropna=mtm_20_dropna.drop('2010-10-28')
-mtm_20_dropna=mtm_20_dropna.drop('2011-01-30')
-mtm_20_dropna=mtm_20_dropna.drop('2011-01-31')
-mtm_20_dropna=mtm_20_dropna.drop('2020-01-19')
-mtm_20_dropna=mtm_20_dropna.drop('2020-01-20')
+mtm_20['stock_mtm_max']=mtm_20.idxmax(axis=1)
 
-for i in mtm_20_dropna.iterrows():
-    mtm_20_dropna.loc[i[0],'test']=etf_all.loc[i[0],mtm_20_dropna.loc[i[0],'max']]
+etf_open=load_obj('etf_open')
+etf_open_pandas=pd.concat(etf_open, axis=1)
 
-money=1
-flag=0
-res={}
-for i in zip(mtm_20_dropna.index,mtm_20_dropna['max_shift_1'],mtm_20_dropna['test']):
-    leiji=etf_all.loc[i[0],i[1]]
-    if i[1]!=flag:
-       fene=money/leiji
-    elif i[1]==flag:
-        fene=fene
-        money=leiji*fene
-    flag=i[1]
-    res[i]=money
+
+mtm_20['stock_hold']=mtm_20['stock_mtm_max'].shift(1)
+
+for i in mtm_20.iterrows():
+
 

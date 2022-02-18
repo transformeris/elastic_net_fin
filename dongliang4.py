26bdea362121898241ed3778bb24f1b9cbad7171
1412189000@qq.com <transformeris>
2022-02-18 15:55:17 +0800

    test connect

--- dongliang4.py
+++ dongliang4.py
@@ -9,7 +9,7 @@ import bs4
 import akshare as ak
 import pickle
 import numpy as np
-
+from datetime import datetime
 def save_obj(obj, name):
     with open(name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
@@ -26,7 +26,8 @@ etf_all=etf_all.sort_index()
 
 
 
-delta_etf_all=etf_all.shift(20)
+
+delta_etf_all=etf_all.shift(10)
 mtm_20=(etf_all-delta_etf_all)/etf_all
 mtm_20['stock_mtm_max']=mtm_20.idxmax(axis=1)
 
@@ -35,7 +36,33 @@ etf_open_pandas=pd.concat(etf_open, axis=1)
 
 
 mtm_20['stock_hold']=mtm_20['stock_mtm_max'].shift(1)
+mtm_20=mtm_20.dropna(how='all')
+mtm_20=mtm_20.drop(datetime.strptime('2005-03-23', '%Y-%m-%d'),axis=0)
+mtm_20=mtm_20.drop(datetime.strptime('2013-8-30', '%Y-%m-%d'),axis=0)
 
 for i in mtm_20.iterrows():
+    if mtm_20.loc[i[0],'stock_mtm_max']!=mtm_20.loc[i[0],'stock_hold']:
+        mtm_20.loc[i[0], 'hold_change']=1
+    elif mtm_20.loc[i[0],'stock_mtm_max']==mtm_20.loc[i[0],'stock_hold']:
+        mtm_20.loc[i[0], 'hold_change'] = 0
+    try:
+        mtm_20.loc[i[0],'buy_price']=etf_open[i[1]['stock_hold']][i[0]]
+        mtm_20.loc[i[0], 'sell_price'] = etf_close[i[1]['stock_hold']][i[0]]
+    except:
+        pass
+
+flag=0
+for i,j in zip(mtm_20.index,mtm_20['stock_hold']):
+    if j!=flag:
+        mtm_20.loc[i,'buy_sign']=1
+    elif j==flag:
+        mtm_20.loc[i,'buy_sign']=0
+    flag=j
 
 
+res={}
+money=1
+for i in mtm_20.iterrows():
+    if i[1]['buy_sign']==1:
+        mtm_20.loc[i[0],'fenne']=money/i[1]['buy_price']
+    if i[1]['hold_change']==1:

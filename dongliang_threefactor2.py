33f8155e93d6438efda294d2e15e6aa1b1b5cb63
1412189000@qq.com <transformeris>
2022-05-08 14:41:39 +0800

    Default Changelist

--- dongliang_threefactor2.py
+++ dongliang_threefactor2.py
@@ -192,6 +192,37 @@ def single_stock_tradeback(stock_code,etf_kline,money,trade_pay_rate,start_date,
     etf_hold.loc[end_date, '卖出金额_手续费后'] = etf_hold.loc[end_date, '金额'] * (1 - trade_pay_rate)
     return etf_hold,etf_hold.loc[end_date, '金额'] * (1 - trade_pay_rate)
 
+def single_stock_tradeback2(stock_code,etf_kline,money,trade_pay_rate,start_date,end_date):
+
+    '''
+    与1相比，将首日收益定为1倍，表示首日以收盘价买入
+    :param stock_code:  str  股票代码，例：'sz159966'
+    :param etf_kline:  dict  全部etf基金k线，
+    :param money: float  初始买入成本，包含手续费
+    :param trade_pay_rate: float，手续费费率，【0-1】
+    :param start_date: date   回测开始日期，
+    :param end_date: date   回测结束日期，
+    :return: pandas.Dataframe  包含净值等信息的
+    '''
+    etf_kline_stock=etf_kline[stock_code]
+    etf_hold = etf_kline_stock[start_date:end_date]
+    etf_close = etf_hold['close']
+    etf_close_shift = etf_close.shift(1)
+    etf_delta = (etf_close - etf_close_shift) / etf_close_shift
+    etf_delta = etf_delta.drop(start_date)
+    etf_hold['incresing_rate'] = etf_delta
+    etf_hold.loc[start_date, 'incresing_rate'] = (etf_hold.loc[start_date, 'close'] - etf_hold.loc[
+        start_date, 'close']) / etf_hold.loc[start_date, 'close']
+    etf_hold['日增长倍数'] = etf_hold['incresing_rate'] + 1
+    etf_hold['净值倍数'] = etf_hold['日增长倍数'].cumprod()
+    money_after_trade=money* (1 - trade_pay_rate)
+    etf_hold['金额'] = etf_hold['净值倍数'] * money_after_trade
+    etf_hold.loc[start_date, '手续费'] = money * trade_pay_rate
+    etf_hold.loc[end_date, '手续费'] = etf_hold.loc[end_date, '金额'] * trade_pay_rate
+
+    etf_hold.loc[end_date, '卖出金额_手续费后'] = etf_hold.loc[end_date, '金额'] * (1 - trade_pay_rate)
+    return etf_hold,etf_hold.loc[end_date, '金额'] * (1 - trade_pay_rate)
+
 
 def etf_get():
     etf_list = ak.fund_etf_category_sina(symbol="ETF基金")
@@ -304,7 +335,7 @@ if __name__=='__main__':
     etf_close=load_obj('etf_close')
 
 
-    etf_code=[159995,515000,512720,512480,512760,515580,515980,588080,515050,515260,515790,512580,515700,512800,512200,512400,515220,510170,161129,159944,515210,159981,512690,512980,510150,512290,515120,512170,159843,159825,159996,161725,512880,512660,513100,510309,159920,513050,518880]
+    etf_code=[510050,159928,159995,515000,512720,512480,512760,515580,515980,588080,515050,515260,515790,512580,515700,512800,512200,512400,515220,510170,161129,159944,515210,159981,512690,512980,510150,512290,515120,512170,159843,159825,159996,161725,512880,512660,513100,510309,159920,513050,518880]
 
     res=[]
     for i in etf_all.keys():
@@ -324,18 +355,18 @@ if __name__=='__main__':
     # mtm_20 = (etf_all - delta_etf_all) / etf_all
     # mtm_20['stock_mtm_max'] = mtm_20.idxmax(axis=1)
 
-    test_etf=etf_close2['sz159944']
+    test_etf=etf_close2['sh510050']
     test_etf_shift=test_etf.shift(1)
     delta=(test_etf-test_etf_shift)/test_etf_shift
     delta1=delta.shift(1)
     delta2 = delta.shift(2)
     mai=0
     flag={}
-    zz=delta.rolling(window=4)
+    zz=delta.rolling(window=6)
     pd.DataFrame(zz)
     for i in zz:
         win=i.drop(min(i.index))
-        if i.loc[min(i.index)]>0 and (win<0).all():
+        if i.loc[min(i.index)]>0 and (win<0.001).all():
             flag[max(i.index)]=1
     flag_pand = pd.DataFrame(flag, index=['买入']).T
     zzz = pd.concat([delta, flag_pand], axis=1)
@@ -354,10 +385,37 @@ if __name__=='__main__':
         if zzz.loc[min(i.index), '持有']==1 and zzz.loc[max(i.index), 'close']<0:
             zzz.loc[max(i.index), '卖出'] = 1
 
-    hold_date=zzz['买入']==1 or zzz['持有']==1 or zzz['卖出']==1
+    hold_date=zzz[zzz['买入']==1]
+    zzz['持有时间段']=zzz['买入']
+    zzz['持有时间段'][zzz['持有']==1]=1
+    zzz['持有时间段'][zzz['卖出'] == 1] = 1
 
+    zzzz=zzz['持有时间段']
 
+    res=[]
+    res1=[]
+    for i in zzzz.iteritems():
+        if i[1]==1:
+            res.append(i[0])
+        elif i[1]!=1 and res!=[]:
+            res1.append(res)
+            res=[]
+            # res1.append(res)
+    res=[]
+    qian=[]
+    quxian=1
+    for i in res1:
 
+        start=min(i)
+        end=max(i)
+        jinzi, quxian=single_stock_tradeback2('sh510050', etf_all, quxian, 0.00015, start, end)
+        aa = jinzi['金额']
+        qian.append(aa)
+        res.append(quxian)
+    aaa=pd.concat(qian)
+    plt.plot(aaa)
+    # plt.plot(test_etf)
+    plt.show()
 
 
 

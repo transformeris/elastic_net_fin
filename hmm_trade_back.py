d0f2d903f5a1c55b96b0e6d3b02033050c639e16
1412189000@qq.com <transformeris>
2022-06-18 22:11:46 +0800

    Default Changelist

--- hmm_trade_back.py
+++ hmm_trade_back.py
@@ -8,10 +8,8 @@ n = 6  # 6个隐藏状态
 # data = pd.read_csv('沪深300.csv', index_col=0)
 data_proto=ak.stock_zh_index_daily(symbol="sh000300")
 data = ak.stock_zh_index_daily(symbol="sh000300")
-
-trade_back=[3665,3915]
-
-data=data_proto[721:]
+# data=data[721:]
+data=data_proto[721:3665]
 data.set_index(data['date'],inplace=True)
 volume = data['volume']
 close = data['close']
@@ -29,10 +27,24 @@ close = close[5:]
 Date = pd.to_datetime(data.index[5:])
 A = np.column_stack([logDel, logRet_5, logVol_5])
 
-# zz=np.count_nonzero(A)
+zz=np.count_nonzero(A)
 
-for i in range(3665,3915):
-    A_hist=A[0:3665]
+model = hmm.GaussianHMM(n_components=n, covariance_type="full", n_iter=2000).fit(A)
+hidden_states = model.predict(A)
 
-    model = hmm.GaussianHMM(n_components=n, covariance_type="full", n_iter=2000).fit(A_hist)
-    hidden_states = model.predict(A)
+# plt.figure(figsize=(25, 18))
+for i in range(n):
+    pos = (hidden_states == i)
+    plt.plot_date(Date[pos], close[pos], 'o', label='hidden state %d' % i, lw=2)
+    plt.legend()
+plt.show()
+
+res = pd.DataFrame({'Date': Date, 'logReg_1': logRet_1, 'state': hidden_states}).set_index('Date')
+series = res.logReg_1
+
+templist = []
+# plt.figure(figsize=(25, 18))
+for i in range(n):
+    pos = (hidden_states == i)
+    pos = np.append(1, pos[:-1])
+    res['state_ret%d' % i] = series.multiply(pos)

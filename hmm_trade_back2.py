ba17b8a7c056ddcd4664c3871db1d9d235a35fd5
1412189000@qq.com <transformeris>
2022-07-16 21:46:46 +0800

    Default Changelist

---
+++ hmm_trade_back2.py
@@ -0,0 +1,51 @@
+from hmmlearn import hmm
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import akshare as ak
+n = 6  # 6个隐藏状态
+
+data_proto=ak.stock_zh_index_daily(symbol="sh000300")
+data = ak.stock_zh_index_daily(symbol="sh000300")
+
+data=data_proto[721:3665]
+data.set_index(data['date'],inplace=True)
+volume = data['volume']
+close = data['close']
+
+logDel = np.log(np.array(data['high'])) - np.log(np.array(data['low']))
+logRet_1 = np.array(np.diff(np.log(close)))
+logRet_5 = np.log(np.array(close[5:])) - np.log(np.array(close[:-5]))
+logVol_5 = np.log(np.array(volume[5:])) - np.log(np.array(volume[:-5]))
+
+logDel = logDel[5:]
+logRet_1 = logRet_1[4:]
+close = close[5:]
+
+Date = pd.to_datetime(data.index[5:])
+A = np.column_stack([logDel, logRet_5, logVol_5])
+
+
+train_collection=A[0:2900]
+model = hmm.GaussianHMM(n_components=n, covariance_type="full", n_iter=2000).fit(train_collection)
+hidden_states = model.predict(train_collection)
+
+res = pd.DataFrame({'logReg_1': logRet_1[0:2900], 'state': hidden_states})
+series = res.logReg_1
+souyi=[]
+for i in range(n):
+    pos = (hidden_states == i)
+    pos = np.append(1, pos[:-1])
+    res['state_ret%d' % i] = series.multiply(pos)
+    data_i = np.exp(res['state_ret%d' % i].cumsum()) #模式i带来的收益倍数
+    souyi.append(data_i[len(data_i)-1])
+    res['data_i%d' % i]=data_i
+
+
+
+
+
+for i in range(2900+1,len(A)):
+    pass
+
+

36deccab3c6b374da34e171eea3d052363e4c7ef
1412189000@qq.com <transformeris>
2022-08-16 11:33:02 +0800

    Default Changelist

--- zhengquanetf_wangge.py
+++ zhengquanetf_wangge.py
@@ -10,9 +10,6 @@ import statsmodels.api as sm
 from copy import deepcopy
 import itertools
 import seaborn as sns
-data_proto=ak.stock_zh_index_daily(symbol="sh000300")
-data = ak.stock_zh_index_daily(symbol="sh000300")
-close=data['close']
 from statsmodels.tsa.stattools import adfuller
 import numpy as np
 import pandas as pd
@@ -32,6 +29,29 @@ if __name__=='__main__':
 
     zhengquan_kline=etf_kline_all['sh512880']
 
+
     zhengquan_kline['jizhunxian']=1
+    zhengquan_kline.loc[datetime.date(2016, 8, 10), 'mairu'] = 1
+    zhengquan_kline.loc[datetime.date(2016, 8, 10), 'mairushoushu'] = 1
+    zhengquan_kline.loc[datetime.date(2016, 8, 10), 'mairujia'] = zhengquan_kline.loc[datetime.date(2016, 8, 10), 'close']
+    zhengquan_kline.loc[datetime.date(2016, 8, 10), 'shouxufeilv'] = 0.001
+    zhengquan_kline.loc[datetime.date(2016, 8, 10), 'mairuchengben'] = (1+zhengquan_kline.loc[datetime.date(2016, 8, 10), 'shouxufeilv'])*zhengquan_kline.loc[datetime.date(2016, 8, 10), 'close']*zhengquan_kline.loc[datetime.date(2016, 8, 10), 'mairushoushu']*100
+    zhengquan_kline.loc[datetime.date(2016, 8, 10), 'shouxufei'] = zhengquan_kline.loc[datetime.date(2016, 8, 10), 'shouxufeilv']*zhengquan_kline.loc[datetime.date(2016, 8, 10), 'mairushoushu']*100*zhengquan_kline.loc[datetime.date(2016, 8, 10), 'mairujia']
+
+
+
+    zhengquan_kline.loc[datetime.date(2016,8,11),'chichang'] = 1
+    zhengquan_kline.loc[datetime.date(2016, 8, 12), 'chichang'] = 1
+    zhengquan_kline.loc[datetime.date(2016, 8, 15), 'chichang'] = 1
+    zhengquan_kline.loc[datetime.date(2016, 8, 17), 'chichang'] = 1
+    zhengquan_kline.loc[datetime.date(2016, 8, 18), 'chichang'] = 1
+    zhengquan_kline['chichang'][zhengquan_kline['chichang']!=1]=0
+
+    zhengquan_kline.loc[datetime.date(2016, 8, 15), 'maichu'] = 1
+
+
+    zhengquan_kline['zhangdie']=zhengquan_kline['close'].diff(1)
+    zhengquan_kline['rijian_zhangdiefu_close'] = zhengquan_kline['close'].diff(1)/zhengquan_kline['close']
+
 
 

d9418263264d695a9b7d4106ecab3ed0529be657
1412189000@qq.com <transformeris>
2022-11-14 20:21:08 +0800

    Default Changelist

--- dongliang_analysis.py
+++ dongliang_analysis.py
@@ -6,9 +6,15 @@ shangzheng.set_index('Unnamed: 0',inplace=True)
 shangzheng['mtm_20']=shangzheng['close']-shangzheng['close'].shift(20)
 shangzheng['close_20']=shangzheng['close'].shift(20)
 shangzheng['fff']=shangzheng['mtm_20']/shangzheng['close_20']*100
+shangzheng['rishouyi']=(shangzheng['close']-shangzheng['open'])/shangzheng['open']
+a=shangzheng.loc['2020-01-04':'2021-01-04','fff']
+a_p=np.percentile(a,[90])
+# ddd=a[a>a_p[0]]
+ddd=a[a>15]
+zz=shangzheng.loc[list(ddd.index),:]
+print(np.cumprod(zz['rishouyi']+1))
 
-a=shangzheng.loc['2005-01-04':'2006-01-04','fff']
-np.percentile(a,[0.95])
+
 # plt.plot(shangzheng.loc[2750:3000,'fff'])
 # plt.hist(shangzheng.loc[2700:2900,'fff'],bins=300)
 # plt.show()

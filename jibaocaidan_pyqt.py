609f09cbf74d5dfd134ff8932ab3c13feefce3e3
1412189000@qq.com <transformeris>
2023-02-14 20:48:58 +0800

    Default Changelist

--- jibaocaidan_pyqt.py
+++ jibaocaidan_pyqt.py
@@ -16,66 +16,66 @@ stacked_widget.addWidget(page3)
 
 # 切换页面
 stacked_widget.setCurrentIndex(1)  # 显示 page2
-# class MainWindow(QWidget):
-#     def __init__(self):
-#         super().__init__()
-#         self.setWindowTitle('Main Window')
-#
-#         # 创建垂直布局
-#         layout = QVBoxLayout()
-#
-#         # 创建QListWidget
-#         self.list_widget = QListWidget()
-#
-#         # 添加几个选项
-#         self.list_widget.addItem('选项1')
-#         self.list_widget.addItem('选项2')
-#         self.list_widget.addItem('选项3')
-#         self.list_widget.addItem('选项4')
-#         self.list_widget.addItem('选项5')
-#
-#         # 绑定按键事件处
-#
-#
-#
-#
-#
-#         self.list_widget.installEventFilter(self)
-#
-#
-#             # 添加QListWidget到布局
-#         layout.addWidget(self.list_widget)
-#
-#     # 设置窗口的布局
-#         self.setLayout(layout)
-#
-#     def eventFilter(self, source, event):
-#         if (event.type() == event.KeyPress and
-#             source is self.list_widget):
-#             key = event.key()
-#             if key == Qt.Key_Down:
-#                 # 向下移动选中项
-#                 current_row = self.list_widget.currentRow()
-#                 if current_row < self.list_widget.count() - 1:
-#                     self.list_widget.setCurrentRow(current_row + 1)
-#             elif key == Qt.Key_Up:
-#                 # 向上移动选中项
-#                 current_row = self.list_widget.currentRow()
-#                 if current_row > 0:
-#                     self.list_widget.setCurrentRow(current_row - 1)
-#             elif key == Qt.Key_Return:
-#                 # 回车键，确定选中的选项
-#                 current_item = self.list_widget.currentItem()
-#                 print('选中的选项：', cu'rrent_item.text())
-#         return super().eventFilter(source, event)
-# if __name__ =='__main__':
-#     app = QApplication(sys.argv)
-#
-#
-#     # 创建主窗口
-#     main_window = MainWindow()
-#
-#     # 显示主窗口
-#     main_window.show()
-#
-#     sys.exit(app.exec_())
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Main Window')
+
+        # 创建垂直布局
+        layout = QVBoxLayout()
+
+        # 创建QListWidget
+        self.list_widget = QListWidget()
+
+        # 添加几个选项
+        self.list_widget.addItem('选项1')
+        self.list_widget.addItem('选项2')
+        self.list_widget.addItem('选项3')
+        self.list_widget.addItem('选项4')
+        self.list_widget.addItem('选项5')
+
+        # 绑定按键事件处
+
+
+
+
+
+        self.list_widget.installEventFilter(self)
+
+
+            # 添加QListWidget到布局
+        layout.addWidget(self.list_widget)
+
+    # 设置窗口的布局
+        self.setLayout(layout)
+
+    def eventFilter(self, source, event):
+        if (event.type() == event.KeyPress and
+            source is self.list_widget):
+            key = event.key()
+            if key == Qt.Key_Down:
+                # 向下移动选中项
+                current_row = self.list_widget.currentRow()
+                if current_row < self.list_widget.count() - 1:
+                    self.list_widget.setCurrentRow(current_row + 1)
+            elif key == Qt.Key_Up:
+                # 向上移动选中项
+                current_row = self.list_widget.currentRow()
+                if current_row > 0:
+                    self.list_widget.setCurrentRow(current_row - 1)
+            elif key == Qt.Key_Return:
+                # 回车键，确定选中的选项
+                current_item = self.list_widget.currentItem()
+                print('选中的选项：', 'current_item.text()')
+        return super().eventFilter(source, event)
+if __name__ =='__main__':
+    app = QApplication(sys.argv)
+
+
+    # 创建主窗口
+    main_window = MainWindow()
+
+    # 显示主窗口
+    main_window.show()
+
+    sys.exit(app.exec_())

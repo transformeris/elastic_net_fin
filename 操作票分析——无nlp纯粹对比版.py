e5d9c38c2a06f401bee5bb2b6400cecb945c9034
1412189000@qq.com <transformeris>
2023-09-17 11:55:08 +0800

    Default Changelist

--- 操作票分析——无nlp纯粹对比版.py
+++ 操作票分析——无nlp纯粹对比版.py
@@ -14,6 +14,7 @@ def extract_string(s, verb_list):
 
 
 data_ = pd.read_excel('D:\新建文件夹 (3)\OneDrive - 7x541z\桌面\操作票/合山全.xlsx')
+data=data_.group_by('操作任务')
 buzhou=np.array(data_['操作步骤'])
 
 verb=['取下','投上','拉开','检查','汇报','再经','合上','断开','将','切换','拆除','核对','投入','测量','在','撤销','复归','退出','确认','记录','对']
@@ -27,43 +28,62 @@ for i in buzhou:
         res.append(i)
 res2=[]
 device_name=[]
+rest=[]
 for i in buzhou:
+    if i.startswith('合上'):
+        heshang_pattern = r'合上(.*)。'
+        match = re.search(heshang_pattern, i)
+        if match:  # 添加一个条件判断以防止没有匹配时产生的错误
+            device_name.append(match.group(1))
+
+
+
     cheliang_pattern = r'测量(.*?)两端对地电位正常'
     match = re.search(cheliang_pattern, i)
     if match:  # 添加一个条件判断以防止没有匹配时产生的错误
         device_name.append(match.group(1))
 
+    #
     toushang_pattern = r'投上(.*)。'
     match = re.search(toushang_pattern, i)
     if match:  # 添加一个条件判断以防止没有匹配时产生的错误
         device_name.append(match.group(1))
+    #
+    # if i.startswith('合上'):
+    #     heshang_pattern = r'合上(.*)。'
+    #     match = re.search(heshang_pattern, i)
+    #     if match:  # 添加一个条件判断以防止没有匹配时产生的错误
+    #         device_name.append(match.group(1))
+    #
 
-    if i.startswith('合上'):
-        heshang_pattern = r'合上(.*)。'
-        match = re.search(heshang_pattern, i)
-        if match:  # 添加一个条件判断以防止没有匹配时产生的错误
-            device_name.append(match.group(1))
-
-    if i.startswith('合上'):
-        heshang_pattern = r'合上(.*)。'
-        match = re.search(heshang_pattern, i)
-        if match:  # 添加一个条件判断以防止没有匹配时产生的错误
-            device_name.append(match.group(1))
-
-
-from docx import Document
-
-# 加载Word文档
-doc = Document('D:\elink下载\WXWorkLocal\File/2023-09\新建文件夹/02 第二册 二次设备部分（合山站）.doc')
-
-# 遍历文档中的所有表格
-for table in doc.tables:
-    # 遍历表格中的所有行
-    for row in table.rows:
-        # 遍历行中的所有单元格
-        for cell in row.cells:
-            print(cell.text)
-
+#
+# from docx import Document
+#
+# # 加载Word文档
+# doc = Document('D:/新建文件夹 (3)/OneDrive - 7x541z/桌面/02 第二册 二次设备部分（合山站）.docx')
+#
+# # 遍历文档中的所有表格
+# for table in doc.tables:
+#     # 遍历表格中的所有行
+#     for row in table.rows:
+#         # 遍历行中的所有单元格
+#         for cell in row.cells:
+#             print(cell.text)
+# # 创建一个空的DataFrame
+# ta_co=[]
+# df = pd.DataFrame()
+#
+# # 遍历文档中的所有表格
+# for i, table in enumerate(doc.tables):
+#     # 对于每个表格，我们将其转化为一个临时的DataFrame
+#     temp_data = [[cell.text for cell in row.cells] for row in table.rows]
+#     temp_df = pd.DataFrame(temp_data)
+#     ta_co.append(temp_df)
+#
+# zz=[]
+# for i in ta_co:
+#     if '压板名称' in i.values or '压板编号' in i.values:
+#         zz.append(i)
 
 
 
